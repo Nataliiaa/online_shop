a942fb660aa0d026d43425a8fe2966fd24a920e9
Oleksandr Khambir <okhambir@teach-len710-02.di.lan>
2018-02-09 21:29:32 +0200

    resize was added

--- algorythms/src/main/java/com/danit/ArrayList.java
+++ algorythms/src/main/java/com/danit/ArrayList.java
@@ -1,15 +1,36 @@
 package com.danit;
 
 public class ArrayList {
+    private int[] array;
+    private int capacity = 10;
+    private int size = 0;
 
-    public ArrayList() {}
+    public ArrayList() {
+        this.array = new int[capacity];
+    }
 
     public void add(int value) {
 
+        if (size >= capacity) {
+            resize();
+        }
+
+        this.array[size] = value;
+        size++;
     }
 
-    public void get(int index) {
-        
+    public int get(int index) {
+        return this.array[index];
     }
 
+    public int size() {
+        return this.size;
+    }
+
+    private void resize() {
+        this.capacity *= 2;
+        int[] newArray = new int[capacity];
+        System.arraycopy(this.array, 0, newArray, 0, size);
+        this.array = newArray;
+    }
 }
--- algorythms/src/test/java/com/danit/ArrayListTest.java
+++ algorythms/src/test/java/com/danit/ArrayListTest.java
@@ -13,4 +13,36 @@ public class ArrayListTest {
         int arrayIndex = 0;
         assertEquals(argValue, list.get(arrayIndex));
     }
+
+    @Test
+    public void verifyArrayListSize() {
+        ArrayList list = new ArrayList();
+
+        int argValue1 = 4;
+        list.add(argValue1);
+        int argValue2 = 5;
+        list.add(argValue2);
+
+        int arraySize = 2;
+        assertEquals(arraySize, list.size());
+    }
+
+    @Test
+    public void verifyArrayListResize() {
+        ArrayList list = new ArrayList();
+        int num = 15;
+
+        for (int i = 0; i < num; i++) {
+            list.add(i);
+        }
+
+        int expectedSize = 15;
+        assertEquals(expectedSize, list.size());
+    }
+
+    @Test
+    public void verifyArrayListRemoveLast() {
+        ArrayList list = new ArrayList();
+        int
+    }
 }
