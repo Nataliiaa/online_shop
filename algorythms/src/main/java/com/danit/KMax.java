c691f80cbd8cc19037096045a1578c30fce7f1ff
student <student@l320st02.di.lan>
2018-02-10 15:46:27 +0200

    KMax

--- algorythms/src/main/java/com/danit/KMax.java
+++ algorythms/src/main/java/com/danit/KMax.java
@@ -1,6 +1,23 @@
 package com.danit;
 
+import java.util.*;
+
 public class KMax {
 
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int k = in.nextInt();
+        PriorityQueue<Integer> queue = new PriorityQueue<Integer>();
+
+        while(in.hasNext()) {
+            int number = in.nextInt();
+            queue.add(number);
+
+            if (queue.size() > k)
+                queue.poll();
+        }
+
+        System.out.println(queue.poll());
+    }
 
 }
