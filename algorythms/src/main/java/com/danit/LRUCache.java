28ec8d004157ce529153e9b8457b5408fff7de63
Alexey Rykhalskiy <alexey.rykhalskiy@gmail.com>
2018-02-21 18:03:36 +0200

    LRU v0.3

--- algorythms/src/main/java/com/danit/LRUCache.java
+++ algorythms/src/main/java/com/danit/LRUCache.java
@@ -3,83 +3,86 @@ package com.danit;
 import java.util.HashMap;
 
 public class LRUCache<K, V> {
+
     class Node<T, U> {
-        T key;
-        U value;
+        private final T key;
+        private final U value;
         Node<T, U> prev;
         Node<T, U> next;
 
-        public Node(Node<T, U> previous, Node<T, U> next, T key, U value){
-            this.prev = previous;
+        Node(Node<T, U> prev, Node<T, U> next, T key, U val){
+            this.prev = prev;
             this.next = next;
             this.key = key;
-            this.value = value;
+            this.value = val;
         }
     }
 
-    private HashMap<K, Node<K, V>> cache;
-    private Node<K, V> leastRU;
-    private Node<K, V> mostRU;
-    private int capacity;
+    private final HashMap<K, Node<K, V>> cache;
+    private Node<K, V> least;
+    private Node<K, V> most;
+    private final int capacity;
     private int size;
 
-    public LRUCache(int sz) {
-        this.capacity = sz;
+    public LRUCache(int capa) {
+        this.cache = new HashMap<>();
+        this.capacity = capa;
         this.size = 0;
-        leastRU = new Node<K, V>(null, null, null, null);
-        mostRU = leastRU;
-        cache = new HashMap<>();
+        this.least = new Node<K, V>(null, null, null, null);
+        this.most = least;
     }
 
     public V get(K key) {
-        Node<K, V> nodeFromCache = cache.get(key);
+        // check whether our item in the cache
+        Node<K, V> node = cache.get(key);
         // no entry in cache
-        if (nodeFromCache == null) {
+        if (node == null) {
             return null;
         }
         // if key is MRU leave the list as it is
-        else if (nodeFromCache.key == mostRU.key) {
-            return mostRU.value;
+        if (node.key == most.key) {
+            return most.value;
         }
-        // Get the next and prev nodes
-        Node<K, V> nextNode = nodeFromCache.next;
-        Node<K, V> prevNode = nodeFromCache.prev;
-        // if at the left-most, we update LRU
-        if (nodeFromCache.key == leastRU.key){
-            nextNode.prev = null;
-            leastRU = nextNode;
+        // now we have to reorder our cache (linked list)
+        // next and prev nodes
+        Node<K, V> next = node.next;
+        Node<K, V> prev = node.prev;
+        if (node.key == least.key) {
+            // if left-most, we update the list and set new least
+            next.prev = null;
+            least = next;
+        } else {
+            // node.key != most.key
+            // we are in the middle, we need to update the items before and after our item
+            prev.next = next;
+            next.prev = prev;
         }
-        // If we are in the middle, we need to update the items before and after our item
-        else if (nodeFromCache.key != mostRU.key){
-            prevNode.next = nextNode;
-            nextNode.prev = prevNode;
-        }
-
-        // Finally move our item to the MRU
-        nodeFromCache.prev = mostRU;
-        mostRU.next = nodeFromCache;
-        mostRU = nodeFromCache;
-        mostRU.next = null;
-        return nodeFromCache.value;
+        // set our node to the most (reorder list)
+        node.prev = most;
+        node.next = null;
+        most.next = node;
+        // update most
+        most = node;
+        return node.value;
     }
 
     public void put(K key, V value) {
         // exit if we are already in the cache
         if (cache.containsKey(key)) return;
         // put the new node at the right-most end of the linked-list
-        Node<K, V> myNode = new Node<K, V>(mostRU, null, key, value);
-        mostRU.next = myNode;
-        cache.put(key, myNode);
-        mostRU = myNode;
+        Node<K, V> node = new Node<K, V>(most, null, key, value);
+        most.next = node;
+        cache.put(key, node);
+        most = node;
         // delete the left-most entry and update the LRU pointer
         if (size == capacity) {
-            cache.remove(leastRU.key);
-            leastRU = leastRU.next;
-            leastRU.prev = null;
+            cache.remove(least.key);
+            least = least.next;
+            least.prev = null;
         } else {
             // size < capacity
             if (size == 0) {
-                leastRU = myNode;
+                least = node;
             }
             size++;
         }
