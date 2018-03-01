0a163ca05ef5b0c0de3e46b2561fb71ebe1df73b
Alexey Rykhalskiy <alexey.rykhalskiy@gmail.com>
2018-03-01 20:00:03 +0200

    Dijkstra Very Verbosed Version

--- algorythms/src/test/java/com/danit/DijkstraAlgorithmVerbosedTest.java
+++ algorythms/src/test/java/com/danit/DijkstraAlgorithmVerbosedTest.java
@@ -2,31 +2,32 @@ package com.danit;
 
 import org.junit.Test;
 
-public class DijkstraAlgorithmBaseTest {
+public class DijkstraAlgorithmVerbosedTest {
     @Test
     public void testDijkstraAlgorithmDistanceOnly() {
         int from = 0;
         int to = 1;
         System.out.println("Distance only:");
-        DijkstraAlgorithmBase d = new DijkstraAlgorithmBase();
+        DijkstraAlgorithmVerbosed d = new DijkstraAlgorithmVerbosed();
 
         System.out.printf("Distance from %s to %s is %d km\n",
-                d.name(from), d.name(to), d.solution(from, to)
+                d.name(from), d.name(to), d.solution_distance(from, to)
         );
     }
 
     @Test
-    public void testDijkstraAlgorithmDistancePath() {
+    public void testDijkstraAlgorithmDistanceAndPath() {
         int from = 0;
         int to = 1;
-        DijkstraAlgorithmBase d = new DijkstraAlgorithmBase();
+        DijkstraAlgorithmVerbosed d = new DijkstraAlgorithmVerbosed();
+        DijkstraAlgorithmVerbosed.Result r = d.solution_smart(from, to);
 
         System.out.println("\nDistance+Path:");
         System.out.printf("Distance from %s to %s is %d km\n",
-                d.name(from), d.name(to), d.solution(from, to)
+                d.name(from), d.name(to), r.distance()
         );
         System.out.printf("Path from %s to %s is %s\n",
-                d.name(from), d.name(to), d.pathReadable(from, to)
+                d.name(from), d.name(to), d.pathReadable(r.path())
         );
 
     }
