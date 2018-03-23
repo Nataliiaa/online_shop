476358d4a4ced763b2982ad680f7b442194450b5
Edward <the.aloner@gmail.com>
2018-03-23 21:26:07 +0200

    html template with jquery #fixed

---
+++ onlineshop/src/main/java/ua/danit/controllers/AssetServlet.java
@@ -0,0 +1,28 @@
+package ua.danit.controllers;
+
+import com.google.common.io.Files;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
+
+@WebServlet (name = "assets", urlPatterns = "/assets/*")
+public class AssetServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String path = req.getPathInfo();
+        File file = new File("resources" + path);
+
+        if (file.exists()) {
+            resp.getWriter().write(Files.toString(file, Charset.defaultCharset()));
+        } else {
+            resp.getWriter().write("Can't find file" + path);
+        }
+    }
+}
--- onlineshop/src/main/java/ua/danit/controllers/ProductPageServlet.java
+++ onlineshop/src/main/java/ua/danit/controllers/ProductPageServlet.java
@@ -9,7 +9,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet (name = "productPage    Servlet", urlPatterns = "/product/")
+@WebServlet (name = "productPageServlet", urlPatterns = "/product")
 public class ProductPageServlet extends HttpServlet {
 
     private String buildProductDescriptionPage(Product product) {
